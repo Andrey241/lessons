6db3d19180b38a8f28604c0521e19d010a01d9b2
Andrey241 <Andrey75.9626@gmail.com>
2022-06-10 18:07:21 +0300

    lesson

--- js/lesson_array.js
+++ js/lesson_array.js
@@ -1,55 +1,125 @@
-const array = ["ivan", "andrey", "samson", "vorfolomeey"];
+// const array = ["ivan", "andrey", "samson", "vorfolomeey"];
 
-// array.filter((item, index, array) => {
-//   item.length >= 5 ? short.push(item) : console.log(item);
+// // array.filter((item, index, array) => {
+// //   item.length >= 5 ? short.push(item) : console.log(item);
+// // });
+
+// const short = array.filter((item) => {
+//   return item.length >= 5;
+// });
+// console.log(short);
+
+// const answers = ["Some", "daDa", "inForMation"];
+
+// const result = answers.map((item, index, array) => {
+//   return item.toLowerCase();
 // });
 
-const short = array.filter((item) => {
-  return item.length >= 5;
-});
-console.log(short);
+// console.log(result);
 
-const answers = ["Some", "daDa", "inForMation"];
+// let reduce = ["Some", "daDa", "inForMation"];
 
-const result = answers.map((item, index, array) => {
-  return item.toLowerCase();
-});
+// reduce = reduce.map((item, index, array) => {
+//   return item.toUpperCase();
+// });
 
-console.log(result);
+// console.log(reduce);
 
-let reduce = ["Some", "daDa", "inForMation"];
+// const arr = [1, 2, 3, 4, 5, 6, 7];
 
-reduce = reduce.map((item, index, array) => {
-  return item.toUpperCase();
-});
+// const arr2 = arr.reduce(function (pV, item, index, array) {
+//   return (pV[index] = array[index]);
+// }, []);
+// console.log(arr2);
 
-console.log(reduce);
+// const str = ["hello ", "world ", "im ", "glade ", "to ", "see ", "you "];
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
+// const str2 = str.reduce((pV, item) => {
+//   return pV + item;
+// });
+// console.log(str2);
 
-const arr2 = arr.reduce(function (pV, item, index, array) {
-  return (pV[index] = array[index]);
-}, []);
-console.log(arr2);
+// const obj = {
+//   ivan: "persone",
+//   ivan2: "persone",
+//   ivan3: "persone",
+//   ivan4: "personepersone",
+//   ivan5: "personepersone",
+// };
 
-const str = ["hello ", "world ", "im ", "glade ", "to ", "see ", "you "];
+// console.log(Object.keys(obj));
 
-const str2 = str.reduce((pV, item) => {
-  return pV + item;
-});
-console.log(str2);
+// const newArr = Object.entries(obj)
+//   .filter((item) => item[1] === "persone")
+//   .map((item, index, array) => item[0]);
+// console.log(newArr);
+// const films = [
+//   {
+//     name: "Titanic",
+//     rating: 9,
+//   },
+//   {
+//     name: "Die hard 5",
+//     rating: 5,
+//   },
+//   {
+//     name: "Matrix",
+//     rating: 8,
+//   },
+//   {
+//     name: "Some bad film",
+//     rating: 4,
+//   },
+// ];
 
-const obj = {
-  ivan: "persone",
-  ivan2: "persone",
-  ivan3: "persone",
-  ivan4: "personepersone",
-  ivan5: "personepersone",
+// function showGoodFilms(arr) {
+//return arr.filter((item) => item["rating"] >= 8);
+//   return arr.reduce(
+//     (acc, curr) => `${typeof acc === "object" ? acc.name : acc}, ${curr.name}`
+//   );
+//   return arr.map((item, index, array) => {
+//     item.id = index;
+//     return item;
+//   });
+// }
+// const id = showGoodFilms(films);
+// function checkFilms(arr) {
+//   return arr.every((item) => (item.id || item.id === 0 ? true : false));
+// }
+// console.log(checkFilms(id));
+// function showListOfFilms(arr) {}
+
+// function setFilmsIds(arr) {}
+
+// const tranformedArray = setFilmsIds(films);
+
+// function checkFilms(arr) {}
+
+const funds = [
+  { amount: -1400 },
+  { amount: 2400 },
+  { amount: -1000 },
+  { amount: 500 },
+  { amount: 10400 },
+  { amount: -11400 },
+];
+
+const getPositiveIncomeAmount = (data) => {
+  return data
+    .filter((item) => item.amount > 0)
+    .reduce((pv, item) => {
+      return (pv += item.amount);
+    }, 0);
 };
 
-console.log(Object.keys(obj));
+console.log(getPositiveIncomeAmount(funds));
+const getTotalIncomeAmount = (data) => {
+  //   return data.some((item) => item.amount < 0)
+  //     ? data.reduce((acc, curr) => acc + curr.amount, 0)
+  //     : getPositiveIncomeAmount(data);
 
-const newArr = Object.entries(obj)
-  .filter((item) => item[1] === "persone")
-  .map((item, index, array) => item[0]);
-console.log(newArr);
+  return data.some((item) => item.amount < 0)
+    ? data.reduce((pv, item) => (pv += item.amount), 0)
+    : getPositiveIncomeAmount(data);
+};
+console.log(getTotalIncomeAmount(funds));
